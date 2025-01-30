d2fb798cbdf61e76a589c72387c2a2bca5ff8fb9
Aaditya Patil <131251920+Aaditatgithub@users.noreply.github.com>
2025-01-30 11:35:50 +0530

    Checkpoint: Refactoring

--- frontend-estiloviento/src/App.tsx
+++ frontend-estiloviento/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UploadPage from './components/Upload';
 import Home from './components/Home';
